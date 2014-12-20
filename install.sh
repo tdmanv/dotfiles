cb1199c5b8bb47b8875bc137c6ad4ae983e532d4
Tom Manville <tdmanville@maginatics.com>
2014-12-20 10:07:12 -0800

    Fixed dotfiles' names.
    Also added setup vimdiff with git.

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-ln -s ./dotfiles/.vimrc .
-ln -s ./dotfiles/.tmux.conf .
-ln -s ./dotfiles/.gitconfig .
-cat ./dotfiles/.bashrc >> .bashrc
+ln -s ./dotfiles/vimrc .vimrc  
+ln -s ./dotfiles/tmux.conf .tmux.conf 
+ln -s ./dotfiles/gitconfig .gitconfig 
+cat ./dotfiles/bashrc >> .bashrc
 git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 vim +BundleInstall +qall
 sudo pip install git+git://github.com/Lokaltog/powerline
